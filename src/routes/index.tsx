ba3b0dfe685321039ff642f6b054aca3cef2eac0
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-12-01 10:57:04 +0100

    chore: demo data for enabling scroll content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,25 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return <h1>home</h1>;
+	const items = generateItems(200);
+	return (
+		<>
+			<h1>home</h1>
+			{items.map((item) => (
+				<p key={item.id}>{item.label}</p>
+			))}
+		</>
+	);
 }
+
+const generateItems = (n: number, from = 0) => {
+	return new Array(n).fill(null).map((_, index) => ({
+		id: index + from,
+		label: `Label ${index + from}`,
+		details: "Details",
+		meta: {
+			selected: false,
+			disabled: false,
+		},
+	}));
+};
