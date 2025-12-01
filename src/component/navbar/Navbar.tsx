a18b1c0074925a709a9f8b09c562dc8c6855ba56
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-12-01 10:58:55 +0100

    feat: move navbar to separate component

---
+++ src/component/navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+	return (
+		<nav className="sticky top-0 z-50 bg-gray-800 text-white px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16 py-4 min-h-[80px] flex items-center">
+			<h1 className="text-xl font-semibold">My Navbar</h1>
+		</nav>
+	);
+};
