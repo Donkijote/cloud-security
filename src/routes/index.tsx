57c2b87288a5200d0c348632d4342e4a260cd731
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-12-01 12:06:13 +0100

    feat: add sections dummy content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,25 +3,49 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	const items = generateItems(200);
-	return (
-		<>
-			<h1>home</h1>
-			{items.map((item) => (
-				<p key={item.id}>{item.label}</p>
-			))}
-		</>
-	);
+  const items = generateItems(100);
+  return (
+    <>
+      <section id={"home"} className={"pt-20"}>
+        <h1>home</h1>
+
+        {items.map((item) => (
+          <p key={item.id}>{item.label}</p>
+        ))}
+      </section>
+      <section id={"services"} className={"pt-20"}>
+        <h1>Services</h1>
+
+        {items.map((item) => (
+          <p key={item.id}>{item.label}</p>
+        ))}
+      </section>
+      <section id={"about"} className={"pt-20"}>
+        <h1>About Us</h1>
+
+        {items.map((item) => (
+          <p key={item.id}>{item.label}</p>
+        ))}
+      </section>
+      <section id={"contact"} className={"pt-20"}>
+        <h1>Contact</h1>
+
+        {items.map((item) => (
+          <p key={item.id}>{item.label}</p>
+        ))}
+      </section>
+    </>
+  );
 }
 
 const generateItems = (n: number, from = 0) => {
-	return new Array(n).fill(null).map((_, index) => ({
-		id: index + from,
-		label: `Label ${index + from}`,
-		details: "Details",
-		meta: {
-			selected: false,
-			disabled: false,
-		},
-	}));
+  return new Array(n).fill(null).map((_, index) => ({
+    id: index + from,
+    label: `Label ${index + from}`,
+    details: "Details",
+    meta: {
+      selected: false,
+      disabled: false,
+    },
+  }));
 };
