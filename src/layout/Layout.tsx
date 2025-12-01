27f3d48182b223e7cf43be3a3d4bf14a73af5718
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-12-01 13:50:16 +0100

    fix: ident

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,18 +2,20 @@ import type { PropsWithChildren } from "react";
 
 import { Navbar } from "@/component/navbar/Navbar";
 
+const year = new Date().getFullYear();
+
 export const Layout = ({ children }: PropsWithChildren) => {
-	return (
-		<div className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-[var(--color-bg)] text-[var(--color-text)]">
-			<Navbar />
+  return (
+    <div className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-[var(--color-bg)] text-[var(--color-text)]">
+      <Navbar />
 
-			<main className="px-4 sm:px-6 lg:px-8 xl:px-12 py-6 bg-[var(--color-surface)] text-[var(--color-text)]">
-				{children}
-			</main>
+      <main className="px-4 sm:px-6 lg:px-8 xl:px-12 py-6 bg-[var(--color-surface)] text-[var(--color-text)]">
+        {children}
+      </main>
 
-			<footer className="px-4 sm:px-6 lg:px-8 xl:px-12 py-4 text-center bg-[var(--color-primary-800)] text-[var(--color-text)]">
-				<p>My Footer © {new Date().getFullYear()}</p>
-			</footer>
-		</div>
-	);
+      <footer className="px-4 sm:px-6 lg:px-8 xl:px-12 py-4 text-center bg-[var(--color-primary-800)] text-[var(--color-text)]">
+        <p>My Footer © {year}</p>
+      </footer>
+    </div>
+  );
 };
