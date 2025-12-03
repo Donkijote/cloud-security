6716ece502cedfba0ba9cf5bbb74e72c5fdb7128
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-12-03 22:02:46 +0100

    feat: add service page layout

--- src/routes/services/$id.tsx
+++ src/routes/services/$id.tsx
@@ -1,6 +1,9 @@
+import { clsx } from "clsx";
+
 import { createFileRoute } from "@tanstack/react-router";
 
 import { getServiceById } from "@/api/get-service-by-id";
+import { Hero } from "@/component/hero/Hero";
 
 export const Route = createFileRoute("/services/$id")({
   component: ServicePage,
@@ -12,6 +15,58 @@ export const Route = createFileRoute("/services/$id")({
 
 function ServicePage() {
   const service = Route.useLoaderData();
-  console.log("ServicePage", service);
-  return <div>Hello "/services/$id"!</div>;
+
+  return (
+    <div className="-mt-20">
+      <Hero imageSrc={service.heroUrl} imageAlt={service.title} />
+      <div className="relative z-10 pt-[420px] lg:pt-[520px] xl:pt-[600px]">
+        <div className="grid gap-16 grid-cols-1">
+          <section
+            id={"title"}
+            className="w-full px-4 py-12 sm:py-16 lg:py-18 text-[var(--color-text)]"
+          >
+            <div className="mx-auto max-w-5xl text-center space-y-6">
+              <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-[var(--color-text)]">
+                {service.title}
+              </h2>
+            </div>
+          </section>
+        </div>
+        {service.sections.map((section, index) => (
+          <section
+            key={section.title}
+            className="py-20 md:py-28 text-[var(--color-text)]"
+          >
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="grid items-center gap-12 md:grid-cols-2">
+                <div
+                  className={clsx(
+                    "relative h-72 sm:h-80 md:h-full rounded-3xl overflow-hidden shadow-lg bg-[var(--color-surface)]",
+                    `order-${index + 1}`,
+                  )}
+                >
+                  <img
+                    src={section.imgUrl}
+                    alt={section.title}
+                    className="h-full w-full object-cover"
+                  />
+                  <div className="absolute inset-0 bg-[var(--color-primary)]/20" />
+                </div>
+
+                <div className="flex flex-col justify-start">
+                  <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center">
+                    {section.title}
+                  </h2>
+
+                  <p className="mt-4 text-[var(--color-text-muted)] leading-relaxed text-center">
+                    {section.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+        ))}
+      </div>
+    </div>
+  );
 }
