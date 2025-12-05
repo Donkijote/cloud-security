3cd17b98dc4f3da5fe93f2a95c4b8ad6abb90eab
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-12-05 15:06:57 +0100

    feat: update favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,52 +9,57 @@ import { Layout } from "@/layout/Layout";
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
-	head: () => ({
-		meta: [
-			{
-				charSet: "utf-8",
-			},
-			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
-			},
-			{
-				title: "TanStack Start Starter",
-			},
-		],
-		links: [
-			{
-				rel: "stylesheet",
-				href: appCss,
-			},
-		],
-	}),
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Cloud Security",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
 
-	shellComponent: RootDocument,
+  shellComponent: RootDocument,
 });
 
-function RootDocument({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<title></title>
-				<HeadContent />
-			</head>
-			<body>
-				<Layout>{children}</Layout>
-				<TanStackDevtools
-					config={{
-						position: "bottom-right",
-					}}
-					plugins={[
-						{
-							name: "Tanstack Router",
-							render: <TanStackRouterDevtoolsPanel />,
-						},
-					]}
-				/>
-				<Scripts />
-			</body>
-		</html>
-	);
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <head>
+        <title></title>
+        <link
+          rel="icon"
+          href="/favicon.svg"
+          media="(prefers-color-scheme: dark)"
+        />
+        <HeadContent />
+      </head>
+      <body>
+        <Layout>{children}</Layout>
+        <TanStackDevtools
+          config={{
+            position: "bottom-right",
+          }}
+          plugins={[
+            {
+              name: "Tanstack Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
